80a06125d25243e24fac9ab292457b97c9bade0a
AshtonGlover <113224225+AshtonGlover@users.noreply.github.com>
2024-08-10 21:16:03 -0400

    feat: copying over base code

---
+++ NPSSuggestions/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  NPSSuggestions
+//
+//  Created by Ashton Glover on 8/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.green, .cyan], startPoint: .topLeading, endPoint: .bottomTrailing))
+            
+            VStack(spacing: 20) {
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(y: -200)
+                
+                TextField("Email", text: $email)
+                    .foregroundColor(.white)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    //sign up
+                } label: {
+                    Text("Sign up")
+                        .foregroundColor(.white)
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.red, .pink], startPoint: .top, endPoint: .bottomTrailing))
+                        )
+                }
+                .padding(.top)
+                .offset(y: 100)
+                
+                Button {
+                    //login
+                } label: {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                        .underline()
+                }
+                .padding(.top)
+                .offset(y: 110)
+            }
+            .frame(width:350)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
